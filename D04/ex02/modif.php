d29c2a28384d96c89fc6d314b79d3d533d6f3b1f
TshireletsoMentor <tmentor@student.wethinkcode.co.za>
2019-10-14 22:06:20 +0200

    added

---
+++ D04/ex02/modif.php
@@ -0,0 +1,21 @@
+<?PHP
+    if(file_exists('../private') && file_exists('../private/passwd') && $_POST['login'] && $_POST['oldpw']
+    && $_POST['newpw'] && $_POST['submit'] == "OK"){
+        $user = unserialize(file_get_contents('../private/passwd'));
+        foreach($user as $key => $var){
+            if ($var['passwd'] == $_POST['oldpw'] && $_POST['newpw'] != $var['passwd']){
+                $arr['login'] = $_POST['login'];
+                $arr['passwd'] = hash('whirlpool', serialize($_POST['newpw']));
+                $user[] = $arr;
+                file_put_contents('../private/passwd', serialize($user));
+                echo "OK\n";
+            }
+            else
+                echo "ERROR\n";
+        }
+    }
+    else
+        echo "ERROR\n";
+
+
+?>
